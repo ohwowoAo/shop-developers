6e30158499d579034837b9cb53df2e2f306156bb
EJ <108451256+ohwowoAo@users.noreply.github.com>
2025-10-16 16:55:45 +0900

    Delete app directory

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import NewProductPage from "./products/new/page";
-
-export default function Home() {
-  return (
-    <div className="bg-white h-full w-full">
-      <NewProductPage />
-    </div>
-  );
-}
